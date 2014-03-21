96a7e3e124d93b372c7e31849555eb75c297decb
Jacob Groundwater <jacob@newrelic.com>
2014-03-21 00:20:28 -0700

    test: test multiple signals

--- test/abort.js
+++ test/abort.js
@@ -3,8 +3,8 @@ var test   = require('tap').test;
 var events = require('events');
 var Job    = require('../index.js')();
 
-test(function (t) {
-  t.plan(1);
+test("abort with SIGQUIT", function (t) {
+  t.plan(2);
 
   var tasks = [{
     exec: process.argv[0],
@@ -18,7 +18,30 @@ test(function (t) {
     job.abort('SIGQUIT');
   });
   job.emitter.on('exit', function (info) {
+    t.strictEqual(info.code, null);
     t.equal(info.signal, 'SIGQUIT');
+  });
+
+  while( tasks.length > 0) job.queue(tasks.shift());
+});
+
+test("abort with SIGKILL", function (t) {
+  t.plan(2);
+
+  var tasks = [{
+    exec: process.argv[0],
+    args: ['-e', 'setTimeout(function(){}, 1000)'],
+    envs: process.env
+  }];
+
+  var job = Job.New();
+
+  job.emitter.on('task', function (proc) {
+    job.abort('SIGKILL');
+  });
+  job.emitter.on('exit', function (info) {
+    t.strictEqual(info.code, null);
+    t.equal(info.signal, 'SIGKILL');
   });
 
   while( tasks.length > 0) job.queue(tasks.shift());
